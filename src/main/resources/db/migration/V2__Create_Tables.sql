505f7b2952b23300ddaa968706e329b4ffe77c0d
David Kronhardt <dakr0013@stud.hs-kl.de>
2021-08-10 13:20:28 +0200

    Set hibernate ddl-auto to none to fix axon tracking token getting always reset

--- src/main/resources/db/migration/V2__Create_Tables.sql
+++ src/main/resources/db/migration/V2__Create_Tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE projects(
-    projectId VARCHAR(36) NOT NULL,
-    projectName TEXT NOT NULL,
-    plannedStartDate DATE NOT NULL,
+    project_id VARCHAR(36) NOT NULL,
+    project_name TEXT NOT NULL,
+    planned_start_date DATE NOT NULL,
     deadline DATE NOT NULL,
-    PRIMARY KEY(projectId)
+    PRIMARY KEY(project_id)
 );
