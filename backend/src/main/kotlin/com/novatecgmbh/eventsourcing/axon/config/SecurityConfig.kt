186cf3798e819f1375f1ef718bad0a2701a34256
David Kronhardt <dakr0013@stud.hs-kl.de>
2021-10-07 12:31:58 +0200

    Configure spring with keycloak

---
+++ backend/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/config/SecurityConfig.kt
@@ -0,0 +1,44 @@
+package com.novatecgmbh.eventsourcing.axon.config
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
+import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+
+
+@KeycloakConfiguration
+internal class SecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
+    @Autowired
+    @Throws(Exception::class)
+    fun configureGlobal(
+        auth: AuthenticationManagerBuilder
+    ) {
+        val keycloakAuthenticationProvider = keycloakAuthenticationProvider()
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
+        auth.authenticationProvider(keycloakAuthenticationProvider)
+    }
+
+    @Bean
+    fun keycloakConfigResolver(): KeycloakSpringBootConfigResolver = KeycloakSpringBootConfigResolver()
+
+    @Bean
+    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy =
+        RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        super.configure(http)
+        http.authorizeRequests()
+            .anyRequest()
+            .authenticated()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
+}
