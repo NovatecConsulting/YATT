776582e74cbe9c448754382404126dd3e04b3b1f
David Kronhardt <dakr0013@stud.hs-kl.de>
2021-10-14 16:00:47 +0200

    Add create task form

--- frontend/src/features/tasks/TaskList.tsx
+++ frontend/src/features/tasks/TaskList.tsx
@@ -1,15 +1,16 @@
 import {ReactJSXElement} from "@emotion/react/types/jsx-namespace";
 import {CircularProgress, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import {selectTaskByProjectIdAndTaskId, useGetTasksByProjectQuery} from "./taskSlice";
-import {useParams} from "react-router-dom";
+import {useHistory, useParams} from "react-router-dom";
 import {useAppSelector} from "../../app/hooks";
 import {EntityId} from "@reduxjs/toolkit";
 import {Scaffold} from "../../components/Scaffold";
 import React from "react";
 import {selectIdsFromResult} from "../../app/rtkQueryHelpers";
+import {TableToolbar} from "../../components/TableToolbar";
 
 export function TaskList() {
-
+    const history = useHistory();
     const {id: projectId} = useParams<{ id: string }>()
 
     const {
@@ -26,6 +27,11 @@ export function TaskList() {
     } else if (isSuccess && taskIds) {
         content = (
             <TableContainer sx={{minWidth: 1000}} component={Paper}>
+                <TableToolbar
+                    title={''}
+                    tooltip={'Create Task'}
+                    onClick={() => history.push(`/projects/${projectId}/tasks/create`)}
+                />
                 <Table stickyHeader>
                     <TableHead>
                         <TableRow>
