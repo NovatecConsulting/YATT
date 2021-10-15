eb2a39e0d633f0f4cfd002f989bbbe7d654774f9
David Kronhardt <dakr0013@stud.hs-kl.de>
2021-10-15 14:51:40 +0200

    Add company ui

---
+++ frontend/src/features/company/CompanyList.tsx
@@ -0,0 +1,87 @@
+import {Company, selectCompanyById, useGetCompaniesQuery} from "./companySlice";
+import {Scaffold} from "../../components/Scaffold";
+import {ReactJSXElement} from "@emotion/react/types/jsx-namespace";
+import {
+    CircularProgress,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@mui/material";
+import React from "react";
+import {selectIdsFromResult} from "../../app/rtkQueryHelpers";
+import {EntityId} from "@reduxjs/toolkit";
+import {useAppSelector} from "../../app/hooks";
+import {TableToolbar} from "../../components/TableToolbar";
+import {useHistory} from "react-router-dom";
+
+export function CompanyList() {
+    const history = useHistory();
+    const {
+        data: companyIds,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetCompaniesQuery(undefined, {selectFromResult: selectIdsFromResult});
+
+    let content: ReactJSXElement;
+    if (isLoading) {
+        content = <CircularProgress/>;
+    } else if (isSuccess && companyIds) {
+        content = (
+            <TableContainer sx={{minWidth: 1000}} component={Paper}>
+                <TableToolbar
+                    title={'All Companies'}
+                    tooltip={'Create Company'}
+                    onClick={() => history.push(`/companies/new`)}
+                />
+                <Table stickyHeader>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            companyIds.map((companyId) => (
+                                <CompanyRow companyId={companyId}/>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        );
+    } else if (isError) {
+        content = <div>{error}</div>;
+    } else {
+        return null;
+    }
+
+    return (
+        <Scaffold>
+            {content}
+        </Scaffold>
+    );
+
+}
+
+interface CompanyRowProps {
+    companyId: EntityId;
+}
+
+function CompanyRow(props: CompanyRowProps) {
+    const company = useAppSelector(state => selectCompanyById(state, props.companyId))
+    if (company) {
+        return (
+            <TableRow hover>
+                <TableCell>{company.name}</TableCell>
+            </TableRow>
+        );
+    } else {
+        return null;
+    }
+}
---
+++ frontend/src/features/company/CreateCompanyForm.tsx
@@ -0,0 +1,61 @@
+import {useSnackbar} from "notistack";
+import {useHistory} from "react-router-dom";
+import {useFormik} from "formik";
+import {Scaffold} from "../../components/Scaffold";
+import {Box, Button, Card, CardContent, TextField} from "@mui/material";
+import React from "react";
+import {useCreateCompanyMutation} from "./companySlice";
+
+interface Values {
+    name: string;
+}
+
+export function CreateCompanyForm() {
+    const {enqueueSnackbar} = useSnackbar();
+    const history = useHistory();
+    const [saveCompany] = useCreateCompanyMutation();
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+        } as Values,
+        validateOnChange: true,
+        onSubmit: async (values, formikHelpers) => {
+            try {
+                await saveCompany({
+                    name: values.name,
+                }).unwrap();
+                enqueueSnackbar(`Company "${values.name}" created successfully`)
+                history.goBack();
+            } catch (e) {
+                // TODO error handling
+                console.log(e)
+                console.log("company creation failed");
+            }
+        },
+    });
+
+    return (
+        <Scaffold>
+            <Card><CardContent>
+                <Box component="form" onSubmit={formik.handleSubmit}>
+                    <TextField
+                        required
+                        fullWidth
+                        id="name"
+                        name="name"
+                        label="Name"
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        error={formik.touched.name && Boolean(formik.errors.name)}
+                        helperText={formik.touched.name && formik.errors.name}
+                    />
+                    <Button variant="contained" fullWidth type="submit"
+                            disabled={formik.isSubmitting || !formik.isValid}>
+                        Submit
+                    </Button>
+                </Box>
+            </CardContent></Card>
+        </Scaffold>
+    );
+}
