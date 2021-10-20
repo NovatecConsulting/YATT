6865f627348949cb5def0bf8381b416b676498e8
David Kronhardt <dakr0013@stud.hs-kl.de>
2021-10-20 11:24:02 +0200

    Add project details projection

--- backend/src/main/resources/db/schema-h2.sql
+++ backend/src/main/resources/db/schema-h2.sql
@@ -69,6 +69,25 @@ create sequence hibernate_sequence start with 1 increment by 1;
         primary key (identifier)
     );
 
+    create table project_by_task_lookup (
+       identifier varchar(255) not null,
+        project_id varchar(255) not null,
+        primary key (identifier)
+    );
+
+    create table project_details (
+       identifier varchar(255) not null,
+        all_tasks_count bigint not null,
+        completed_tasks_count bigint not null,
+        deadline date not null,
+        name varchar(255) not null,
+        planned_start_date date not null,
+        planned_tasks_count bigint not null,
+        started_tasks_count bigint not null,
+        version bigint not null,
+        primary key (identifier)
+    );
+
     create table projects (
        identifier varchar(255) not null,
         deadline date not null,
--- backend/src/main/resources/db/schema-postgres.sql
+++ backend/src/main/resources/db/schema-postgres.sql
@@ -69,6 +69,25 @@ create sequence hibernate_sequence start 1 increment 1;
         primary key (identifier)
     );
 
+    create table project_by_task_lookup (
+       identifier varchar(255) not null,
+        project_id varchar(255) not null,
+        primary key (identifier)
+    );
+
+    create table project_details (
+       identifier varchar(255) not null,
+        all_tasks_count int8 not null,
+        completed_tasks_count int8 not null,
+        deadline date not null,
+        name varchar(255) not null,
+        planned_start_date date not null,
+        planned_tasks_count int8 not null,
+        started_tasks_count int8 not null,
+        version int8 not null,
+        primary key (identifier)
+    );
+
     create table projects (
        identifier varchar(255) not null,
         deadline date not null,
