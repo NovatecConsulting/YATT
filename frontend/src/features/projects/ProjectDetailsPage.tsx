d7a8b1b93473268df18606e3aec7487d71f5911c
David Kronhardt <dakr0013@stud.hs-kl.de>
2021-10-20 12:16:45 +0200

    Add showing statistics in project page

--- frontend/src/features/projects/ProjectDetailsPage.tsx
+++ frontend/src/features/projects/ProjectDetailsPage.tsx
@@ -1,36 +1,70 @@
 import {Scaffold} from "../../components/Scaffold";
-import {Box, Button, Card, CardActions, CardContent, CardHeader, Skeleton} from "@mui/material";
+import {Box, Button, Card, CardActions, CardContent, CardHeader, Skeleton, Typography} from "@mui/material";
 import {useHistory, useParams} from "react-router-dom";
+import {useGetProjectDetailsQuery} from "./projectsSlice";
+import {ReactJSXElement} from "@emotion/react/types/jsx-namespace";
+import React from "react";
 
 export function ProjectDetailsPage() {
     const history = useHistory();
     const {id: projectId} = useParams<{ id: string }>();
 
+    const result = useGetProjectDetailsQuery(projectId);
     const navigateToTaskList = () => history.push(`/projects/${projectId}/tasks`)
     const navigateToParticipantList = () => history.push(`/projects/${projectId}/participants`)
+
+    let taskCardContent: ReactJSXElement | null = null;
+    if (result.isLoading || result.isSuccess) {
+        taskCardContent = (
+            <React.Fragment>
+                <Typography>
+                    {result.isSuccess ? `# Overall Tasks: ${result.data.allTasksCount}` : <Skeleton variant="text"/>}
+                </Typography>
+                <Typography>
+                    {result.isSuccess ? `# Planned Tasks: ${result.data.plannedTasksCount}` :
+                        <Skeleton variant="text"/>}
+                </Typography>
+                <Typography>
+                    {result.isSuccess ? `# Started Tasks: ${result.data.startedTasksCount}` :
+                        <Skeleton variant="text"/>}
+                </Typography>
+                <Typography>
+                    {result.isSuccess ? `# Completed Tasks: ${result.data.completedTasksCount}` :
+                        <Skeleton variant="text"/>}
+                </Typography>
+            </React.Fragment>
+        );
+    } else if (result.isError) {
+        taskCardContent = <div>{result.error}</div>;
+    }
 
     return (
         <Scaffold alignItems='start'>
             <Box className='row'>
-                <Card>
+                <Card sx={{width: 300, display: 'flex', flexDirection: 'column'}}>
                     <CardHeader title='Tasks'/>
-                    <CardContent>
-                        <Skeleton variant="text" width={300}/>
-                        <Skeleton variant="text"/>
-                        <Skeleton variant="text"/>
-                        <Skeleton variant="text"/>
+                    <CardContent sx={{flexGrow: 1}}>
+                        {taskCardContent}
                     </CardContent>
                     <CardActions>
                         <Button onClick={navigateToTaskList}>View Tasks</Button>
                     </CardActions>
                 </Card>
-                <Card>
+                <Card sx={{width: 300, display: 'flex', flexDirection: 'column'}}>
                     <CardHeader title='Participants'/>
-                    <CardContent>
-                        <Skeleton variant="text" width={300}/>
-                        <Skeleton variant="text"/>
-                        <Skeleton variant="text"/>
-                        <Skeleton variant="text"/>
+                    <CardContent sx={{flexGrow: 1}}>
+                        <Typography>
+                            <Skeleton variant="text"/>
+                        </Typography>
+                        <Typography>
+                            <Skeleton variant="text"/>
+                        </Typography>
+                        <Typography>
+                            <Skeleton variant="text"/>
+                        </Typography>
+                        <Typography>
+                            <Skeleton variant="text"/>
+                        </Typography>
                     </CardContent>
                     <CardActions>
                         <Button onClick={navigateToParticipantList}>View Participants</Button>
