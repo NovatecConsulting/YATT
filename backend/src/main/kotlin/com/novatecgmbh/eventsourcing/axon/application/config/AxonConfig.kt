b427bf1ff65a034c99f6922e66cd23a324bc7a6b
David Kronhardt <dakr0013@stud.hs-kl.de>
2021-10-21 15:09:46 +0200

    Add auditing

--- backend/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/application/config/AxonConfig.kt
+++ backend/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/application/config/AxonConfig.kt
@@ -1,8 +1,16 @@
 package com.novatecgmbh.eventsourcing.axon.application.config
 
+import com.novatecgmbh.eventsourcing.axon.application.auditing.AUDIT_KEYS
+import com.novatecgmbh.eventsourcing.axon.application.auditing.AuditingInterceptor
 import com.novatecgmbh.eventsourcing.axon.common.command.ExceptionWrappingHandlerInterceptor
 import org.axonframework.commandhandling.CommandBus
+import org.axonframework.commandhandling.CommandMessage
+import org.axonframework.messaging.correlation.CorrelationDataProvider
+import org.axonframework.messaging.correlation.MessageOriginProvider
+import org.axonframework.messaging.correlation.MultiCorrelationDataProvider
+import org.axonframework.messaging.correlation.SimpleCorrelationDataProvider
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
@@ -13,6 +21,17 @@ class AxonConfig {
       commandBus: CommandBus,
       exceptionWrappingHandlerInterceptor: ExceptionWrappingHandlerInterceptor,
   ) {
-    commandBus.registerHandlerInterceptor(exceptionWrappingHandlerInterceptor)
+    commandBus.run {
+      registerHandlerInterceptor(exceptionWrappingHandlerInterceptor)
+      registerDispatchInterceptor(AuditingInterceptor())
+    }
   }
+
+  @Bean
+  fun correlationDataProviders(): CorrelationDataProvider =
+      MultiCorrelationDataProvider<CommandMessage<*>>(
+          listOf(
+              SimpleCorrelationDataProvider(*AUDIT_KEYS),
+              MessageOriginProvider(),
+          ))
 }
--- backend/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/user/web/UserController.kt
+++ backend/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/user/web/UserController.kt
@@ -5,20 +5,16 @@ import com.novatecgmbh.eventsourcing.axon.application.security.UnregisteredUserP
 import com.novatecgmbh.eventsourcing.axon.user.api.*
 import com.novatecgmbh.eventsourcing.axon.user.web.dto.RegisterUserDto
 import com.novatecgmbh.eventsourcing.axon.user.web.dto.RenameUserDto
-import java.time.Duration
 import java.util.concurrent.CompletableFuture
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.extensions.kotlin.queryMany
 import org.axonframework.extensions.kotlin.queryOptional
-import org.axonframework.messaging.responsetypes.ResponseTypes
 import org.axonframework.queryhandling.QueryGateway
-import org.axonframework.queryhandling.SubscriptionQueryResult
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.*
-import reactor.core.publisher.Mono
 
 @RequestMapping("/v2/users")
 @RestController
@@ -46,32 +42,11 @@ class UserController(
   fun registerUser(
       @RequestBody body: RegisterUserDto,
       @AuthenticationPrincipal principal: UnregisteredUserPrincipal
-  ): Mono<ResponseEntity<UserQueryResult>> =
-      queryGateway.subscriptionQuery(
-              FindUserByExternalUserIdQuery(principal.username),
-              ResponseTypes.instanceOf(UserQueryResult::class.java),
-              ResponseTypes.instanceOf(UserQueryResult::class.java),
-          )
-          .let { queryResult -> wrapCommand(queryResult, body.toCommand(principal.username)) }
+  ): CompletableFuture<UserId> = commandGateway.send(body.toCommand(principal.username))
 
   @PostMapping("/current/rename")
   fun renameUser(
       @RequestBody body: RenameUserDto,
       @AuthenticationPrincipal principal: RegisteredUserPrincipal
-  ): Mono<ResponseEntity<UserQueryResult>> =
-      queryGateway.subscriptionQuery(
-              UserQuery(principal.identifier),
-              ResponseTypes.instanceOf(UserQueryResult::class.java),
-              ResponseTypes.instanceOf(UserQueryResult::class.java),
-          )
-          .let { queryResult -> wrapCommand(queryResult, body.toCommand(principal.identifier)) }
-
-  fun <T, R> wrapCommand(queryResult: SubscriptionQueryResult<T, T>, command: R) =
-      Mono.`when`(queryResult.initialResult())
-          .then(Mono.fromCompletionStage { commandGateway.send<Unit>(command) })
-          .thenMany(queryResult.updates())
-          .next()
-          .map { entity -> ResponseEntity.ok(entity) }
-          .timeout(Duration.ofSeconds(5))
-          .doFinally { queryResult.cancel() }
+  ): CompletableFuture<UserId> = commandGateway.send(body.toCommand(principal.identifier))
 }
