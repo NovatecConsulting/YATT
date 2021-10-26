7befb9f34eb95666bdd83191ae69dc1ac19037b4
David Kronhardt <dakr0013@stud.hs-kl.de>
2021-10-26 17:23:30 +0200

    Add run config for 2. instance; Remove volume binding for event data

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = Keycloak({
-    url: 'http://localhost:8081/auth',
+    url: 'http://localhost:8999/auth',
     realm: 'eventsourcing-with-axon',
     clientId: 'my-backend'
 });
