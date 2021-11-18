71f0a1a5b3f77b30c4a5b3ab9e2dbe2d25c9c2d7
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2021-11-18 16:58:23 +0100

    Add root context id if applying event in todo entity
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

--- backend/project/command-query/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/project/task/command/Todo.kt
+++ backend/project/command-query/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/project/task/command/Todo.kt
@@ -5,8 +5,11 @@ import com.novatecgmbh.eventsourcing.axon.project.task.api.TodoId
 import com.novatecgmbh.eventsourcing.axon.project.task.api.TodoMarkedAsDoneEvent
 import org.axonframework.commandhandling.CommandHandler
 import org.axonframework.eventsourcing.EventSourcingHandler
+import org.axonframework.messaging.MetaData
+import org.axonframework.messaging.Scope
 import org.axonframework.modelling.command.AggregateLifecycle
 import org.axonframework.modelling.command.EntityId
+import org.axonframework.modelling.command.inspection.AnnotatedAggregate
 
 class Todo(entityIdentifier: TodoId, private var description: String, isDone: Boolean) {
   @EntityId(routingKey = "todoId")
@@ -20,7 +23,11 @@ class Todo(entityIdentifier: TodoId, private var description: String, isDone: Bo
   fun handle(command: MarkTodoAsDoneCommand) {
     if (!isDone) {
       // TODO rootContextId
-      AggregateLifecycle.apply(TodoMarkedAsDoneEvent(command.identifier, command.todoId))
+      Scope.getCurrentScope<AnnotatedAggregate<Task>>().execute {
+        AggregateLifecycle.apply(
+            TodoMarkedAsDoneEvent(command.identifier, command.todoId),
+            MetaData(mapOf("rootContextId" to it.getRootContextId())))
+      }
     }
   }
 
