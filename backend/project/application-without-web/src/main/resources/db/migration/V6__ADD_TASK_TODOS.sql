2f68ca90ab2e96deef09534070352f5ff413794f
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2021-11-24 10:13:20 +0100

    Change task projection to contain todos as element collection
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

--- backend/project/application-without-web/src/main/resources/db/migration/V6__ADD_TASK_TODOS.sql
+++ backend/project/application-without-web/src/main/resources/db/migration/V6__ADD_TASK_TODOS.sql
@@ -1,15 +1,17 @@
 create table task_todos
 (
-    task_id     varchar(255) not null,
-    todo_id     varchar(255) not null,
-    description varchar(255),
-    is_done     boolean      not null,
-    primary key (task_id, todo_id)
+    task_identifier varchar(255) not null,
+    description     varchar(255) not null,
+    is_done         boolean      not null,
+    identifier      varchar(255)
 );
 
 alter table if exists task_todos
-    add constraint FK_TaskTodo_taskId
-        foreign key (task_id)
+    add constraint UK_TaskTodos_Identifier unique (identifier, task_identifier);
+
+alter table if exists task_todos
+    add constraint FK_TaskTodos_TaskIdentifier
+        foreign key (task_identifier)
             references tasks;
 
-create index IX_TaskTodo_Task on task_todos (task_id);
+create index IX_TaskTodo_TaskIden on task_todos (task_identifier);
