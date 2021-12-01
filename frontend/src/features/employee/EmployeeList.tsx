d22480ccbc00634180208f9e70f446be19b8c798
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2021-12-01 13:57:17 +0100

    Refactor employee table
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

--- frontend/src/features/employee/EmployeeList.tsx
+++ frontend/src/features/employee/EmployeeList.tsx
@@ -1,75 +1,83 @@
 import {ReactJSXElement} from "@emotion/react/types/jsx-namespace";
 import {
+    Box,
     CircularProgress,
     Paper,
     Switch,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow
 } from "@mui/material";
 import {useHistory, useParams} from "react-router-dom";
-import {useAppSelector} from "../../app/hooks";
-import {EntityId} from "@reduxjs/toolkit";
 import {Scaffold} from "../../components/scaffold/Scaffold";
 import React from "react";
-import {selectIdsFromResult} from "../../app/rtkQueryHelpers";
+import {selectEntitiesFromResult} from "../../app/rtkQueryHelpers";
 import {TableToolbar} from "../../components/TableToolbar";
 import {
-    selectEmployeesByCompanyIdAndEmployeeId,
+    Employee,
     useGetEmployeesByCompanyQuery,
     useGrantAdminPermissionMutation,
     useGrantProjectManagerPermissionMutation,
     useRemoveAdminPermissionMutation,
     useRemoveProjectManagerPermissionMutation
 } from "./employeeSlice";
+import {VirtualizedTable} from "../../components/VirtualizedTable";
 
 export function EmployeeList() {
     const history = useHistory();
     const {companyId} = useParams<{ companyId: string }>();
 
     const {
-        data: employeeIds,
+        data: employees,
         isLoading,
         isSuccess,
         isError,
         error
-    } = useGetEmployeesByCompanyQuery(companyId, {selectFromResult: selectIdsFromResult});
+    } = useGetEmployeesByCompanyQuery(companyId, {selectFromResult: selectEntitiesFromResult});
 
     const navigateToEmployeeCreateForm = () => history.push(`/companies/${companyId}/employees/new`)
 
     let content: ReactJSXElement;
     if (isLoading) {
         content = <CircularProgress/>;
-    } else if (isSuccess && employeeIds) {
+    } else if (isSuccess && employees) {
         content = (
-            <TableContainer sx={{maxWidth: 1000}} component={Paper}>
+            <Paper sx={{width: 1000, display: "flex", flexFlow: "column", flex: "1 1 auto"}}>
                 <TableToolbar
-                    title={``}
+                    title={'Employees'}
                     tooltip={'Create Employee'}
                     onClick={navigateToEmployeeCreateForm}
                 />
-                <Table stickyHeader>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>First Name</TableCell>
-                            <TableCell>Last Name</TableCell>
-                            <TableCell>Is Admin</TableCell>
-                            <TableCell>Is Project Manager</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            employeeIds.map(
-                                (employeeId: EntityId) => <EmployeeRow key={employeeId} companyId={companyId}
-                                                                       employeeId={employeeId}/>
-                            )
-                        }
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                <Box sx={{flex: "1 1 auto"}}>
+                    <VirtualizedTable
+                        rowHeight={64}
+                        rowCount={employees.length}
+                        rowGetter={(index) => employees[index.index]}
+                        columns={[
+                            {
+                                width: 120,
+                                label: "First Name",
+                                dataKey: "userFirstName",
+                            },
+                            {
+                                width: 120,
+                                label: "Last Name",
+                                dataKey: "userLastName",
+                            },
+                            {
+                                width: 120,
+                                label: "Is Admin",
+                                dataKey: "isAdmin",
+                                cellRenderer: (cellProps) => <IsAdminCell employee={cellProps.rowData as Employee}/>
+                            },
+                            {
+                                width: 120,
+                                label: "Is Project Manager",
+                                dataKey: "isProjectManager",
+                                cellRenderer: (cellProps) => <IsProjectManagerCell
+                                    employee={cellProps.rowData as Employee}/>
+                            },
+                        ]}
+                    />
+                </Box>
+            </Paper>
         );
     } else if (isError) {
         content = <div>{error}</div>;
@@ -84,45 +92,38 @@ export function EmployeeList() {
     );
 }
 
-interface EmployeeRowProps {
-    companyId: EntityId;
-    employeeId: EntityId;
-}
-
-function EmployeeRow(props: EmployeeRowProps) {
-    const employee = useAppSelector((state) => selectEmployeesByCompanyIdAndEmployeeId(state, props.companyId, props.employeeId))
+function IsAdminCell({employee}: { employee: Employee }) {
     const [grantAdminPermission] = useGrantAdminPermissionMutation();
     const [removeAdminPermission] = useRemoveAdminPermissionMutation();
+
+    return (
+        <React.Fragment>
+            {employee.isAdmin ? 'yes' : 'no'}
+            <Switch checked={employee.isAdmin} onChange={(event) => {
+                if (event.target.checked) {
+                    grantAdminPermission(employee.identifier);
+                } else {
+                    removeAdminPermission(employee.identifier)
+                }
+            }}/>
+        </React.Fragment>
+    );
+}
+
+function IsProjectManagerCell({employee}: { employee: Employee }) {
     const [grantProjectMangerPermission] = useGrantProjectManagerPermissionMutation();
     const [removeProjectMangerPermission] = useRemoveProjectManagerPermissionMutation();
 
-    if (employee)
-        return (
-            <TableRow hover>
-                <TableCell>{employee.userFirstName}</TableCell>
-                <TableCell>{employee.userLastName}</TableCell>
-                <TableCell>
-                    {employee.isAdmin ? 'yes' : 'no'}
-                    <Switch checked={employee.isAdmin} onChange={(event) => {
-                        if (event.target.checked) {
-                            grantAdminPermission(employee.identifier);
-                        } else {
-                            removeAdminPermission(employee.identifier)
-                        }
-                    }}/>
-                </TableCell>
-                <TableCell>
-                    {employee.isProjectManager ? 'yes' : 'no'}
-                    <Switch checked={employee.isProjectManager} onChange={(event) => {
-                        if (event.target.checked) {
-                            grantProjectMangerPermission(employee.identifier);
-                        } else {
-                            removeProjectMangerPermission(employee.identifier)
-                        }
-                    }}/></TableCell>
-            </TableRow>
-        );
-    else {
-        return null;
-    }
+    return (
+        <React.Fragment>
+            {employee.isProjectManager ? 'yes' : 'no'}
+            <Switch checked={employee.isProjectManager} onChange={(event) => {
+                if (event.target.checked) {
+                    grantProjectMangerPermission(employee.identifier);
+                } else {
+                    removeProjectMangerPermission(employee.identifier)
+                }
+            }}/>
+        </React.Fragment>
+    );
 }
