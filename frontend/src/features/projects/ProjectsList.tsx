600a5df79cca29fe209dc873c7cd4c3ac75b74c4
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2021-12-01 18:51:01 +0100

    Refactor project table
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

--- frontend/src/features/projects/ProjectsList.tsx
+++ frontend/src/features/projects/ProjectsList.tsx
@@ -1,74 +1,107 @@
 import {
-    CircularProgress,
+    Box,
+    CircularProgress, IconButton,
     Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
+    Typography,
 } from "@mui/material";
 import {
     Project,
     ProjectStatus,
-    selectProjectById,
     useGetProjectsQuery,
     useRenameProjectMutation,
     useRescheduleProjectMutation
 } from "./projectsSlice";
 import {ReactJSXElement} from "@emotion/react/types/jsx-namespace";
-import {useAppSelector} from "../../app/hooks";
-import {EntityId} from "@reduxjs/toolkit";
 import {Scaffold} from "../../components/scaffold/Scaffold";
-import {selectIdsFromResult} from "../../app/rtkQueryHelpers";
-import React from "react";
+import {selectEntitiesFromResult} from "../../app/rtkQueryHelpers";
+import React, {useState} from "react";
 import {useHistory} from "react-router-dom";
 import {TableToolbar} from "../../components/TableToolbar";
 import {EditableText} from "../../components/EditableText";
-import {EditableDateTableCells} from "../../components/EditableDatesTableCell";
+import {RescheduleDialog} from "../../components/EditableDatesTableCell";
 import dayjs from "dayjs";
+import {VirtualizedTable} from "../../components/VirtualizedTable";
+import {Edit} from "@mui/icons-material";
 
 export function ProjectsList() {
     const history = useHistory();
     const {
-        data: projectIds,
+        data: projects,
         isLoading,
         isSuccess,
         isError,
         error
-    } = useGetProjectsQuery(undefined, {selectFromResult: selectIdsFromResult});
+    } = useGetProjectsQuery(undefined, {selectFromResult: selectEntitiesFromResult});
+
+    const [projectToReschedule, setProjectToReschedule] = useState<Project | undefined>(undefined);
+
+    const closeRescheduleDialog = () => setProjectToReschedule(undefined);
+
+    const navigateToCreateProjectForm = () => history.push(`/projects/new`);
+
+    const navigateToProjectDetailsPage = (projectId: string) => history.push(`/projects/${projectId}`)
 
     let content: ReactJSXElement;
     if (isLoading) {
         content = <CircularProgress/>;
-    } else if (isSuccess && projectIds) {
+    } else if (isSuccess && projects) {
         content = (
-            <TableContainer sx={{maxWidth: 1000}} component={Paper}>
+            <Paper sx={{width: 1000, height: 500, display: "flex", flexFlow: "column", flex: "1 1 auto"}}>
                 <TableToolbar
                     title={'All Projects'}
                     tooltip={'Create Project'}
-                    onClick={() => history.push(`/projects/new`)}
+                    onClick={navigateToCreateProjectForm}
                 />
-                <Table stickyHeader>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Company</TableCell>
-                            <TableCell>Planned Start Date</TableCell>
-                            <TableCell>Deadline</TableCell>
-                            <TableCell>Status</TableCell>
-                            <TableCell>Actual End Date</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            projectIds.map(
-                                (projectId: EntityId) => <ProjectRow key={projectId} projectId={projectId}/>
-                            )
-                        }
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                <Box sx={{flex: "1 1 auto"}}>
+                    <VirtualizedTable
+                        rowHeight={64}
+                        rowCount={projects.length}
+                        rowGetter={(index) => projects[index.index]}
+                        onRowClick={(row) => navigateToProjectDetailsPage((row.rowData as Project).identifier)}
+                        columns={[
+                            {
+                                width: 120,
+                                label: "Name",
+                                dataKey: "name",
+                                cellRenderer: (cellProps) => <ProjectNameCell project={cellProps.rowData as Project}/>
+                            },
+                            {
+                                width: 120,
+                                label: "Company",
+                                dataKey: "companyReference",
+                                cellRenderer: (cellProps) => <CompanyNameCell project={cellProps.rowData as Project}/>
+                            },
+                            {
+                                width: 120,
+                                label: "Planned Start Date",
+                                dataKey: "startDate",
+                                cellRenderer: (cellProps) => <DateCell project={cellProps.rowData as Project}
+                                                                       isStartDate={true}
+                                                                       onEdit={setProjectToReschedule}/>
+                            },
+                            {
+                                width: 120,
+                                label: "Deadline",
+                                dataKey: "deadline",
+                                cellRenderer: (cellProps) => <DateCell project={cellProps.rowData as Project}
+                                                                       onEdit={setProjectToReschedule}/>
+                            },
+                            {
+                                width: 120,
+                                label: "Status",
+                                dataKey: "status",
+                                cellRenderer: (cellProps) => <ProjectStatusCell project={cellProps.rowData as Project}/>
+                            },
+                            {
+                                width: 120,
+                                label: "Actual End Date",
+                                dataKey: "actualEndDate",
+                                cellRenderer: (cellProps) => <ActualEndDateCell project={cellProps.rowData as Project}/>
+                            },
+                        ]}
+                    />
+                </Box>
+            </Paper>
         );
     } else if (isError) {
         content = <div>{error}</div>;
@@ -79,54 +112,19 @@ export function ProjectsList() {
     return (
         <Scaffold>
             {content}
+            <ProjectRescheduleDialog
+                key={projectToReschedule?.identifier}
+                project={projectToReschedule}
+                handleClose={closeRescheduleDialog}
+            />
         </Scaffold>
     );
 }
 
-interface ProjectRowProps {
-    projectId: EntityId;
-}
-
-function ProjectRow(props: ProjectRowProps) {
-    const project = useAppSelector(state => selectProjectById(state, props.projectId))
-    const history = useHistory()
-    const [rescheduleProject] = useRescheduleProjectMutation();
-
-    const navigateToProjectDetailsPage = () => history.push(`/projects/${project?.identifier}`)
-
-    const onSave = async (startDate: string, endDate: string) => {
-        await rescheduleProject({
-            identifier: project!.identifier,
-            version: project!.version,
-            startDate: startDate,
-            deadline: endDate,
-        }).unwrap();
-    }
-
-    if (project) {
-        return (
-            <TableRow hover onClick={navigateToProjectDetailsPage}>
-                <ProjectNameCell project={project}/>
-                <TableCell>{project.companyReference.displayName}</TableCell>
-                <EditableDateTableCells
-                    canEdit={true}
-                    onSave={onSave}
-                    startDate={project.startDate}
-                    endDate={project.deadline}
-                />
-                <ProjectStatusCell status={project.status}/>
-                <TableCell>{project.actualEndDate ? dayjs(project.actualEndDate).format('DD.MM.YYYY') : '-'}</TableCell>
-            </TableRow>
-        );
-    } else {
-        return null;
-    }
-}
-
-function ProjectStatusCell({status}: { status: ProjectStatus }) {
+function ProjectStatusCell({project}: { project: Project }) {
     let color: string;
     let text: string;
-    switch (status) {
+    switch (project.status) {
         case ProjectStatus.DELAYED:
             text = 'Delayed'
             color = 'red';
@@ -137,9 +135,17 @@ function ProjectStatusCell({status}: { status: ProjectStatus }) {
             break;
     }
 
-    return <TableCell sx={{color: color}}>{text}</TableCell>
+    return <Typography variant={"body2"} sx={{color: color}}>{text}</Typography>
 }
 
+function CompanyNameCell({project}: { project: Project }) {
+    return <Typography variant={"body2"}>{project.companyReference.displayName}</Typography>;
+}
+
+function ActualEndDateCell({project}: { project: Project }) {
+    return <Typography
+        variant={"body2"}>{project.actualEndDate ? dayjs(project.actualEndDate).format('DD.MM.YYYY') : '-'}</Typography>;
+}
 
 function ProjectNameCell({project}: { project: Project }) {
     const [saveName] = useRenameProjectMutation();
@@ -153,8 +159,51 @@ function ProjectNameCell({project}: { project: Project }) {
     };
 
     return (
-        <TableCell>
-            <EditableText initialValue={project.name} label={'Name'} canEdit={true} onSave={onSave}/>
-        </TableCell>
+        <EditableText initialValue={project.name} label={'Name'} canEdit={true} onSave={onSave}/>
+    );
+}
+
+interface ProjectRescheduleDialogProps {
+    project?: Project;
+    handleClose: () => void;
+}
+
+function ProjectRescheduleDialog({project, handleClose}: ProjectRescheduleDialogProps) {
+    const [rescheduleProject] = useRescheduleProjectMutation();
+    const isOpen = !!project;
+
+    const onSave = async (startDate: string, endDate: string) => {
+        await rescheduleProject({
+            identifier: project!.identifier,
+            version: project!.version,
+            startDate: startDate,
+            deadline: endDate,
+        }).unwrap();
+    }
+
+    return (
+        <RescheduleDialog initialStartDate={project?.startDate ?? ""}
+                          initialEndDate={project?.deadline ?? ""}
+                          open={isOpen}
+                          onClose={handleClose}
+                          onSave={onSave}/>
+    );
+}
+
+function DateCell({
+                      project,
+                      isStartDate,
+                      onEdit
+                  }: { project: Project, isStartDate?: boolean, onEdit: (project: Project) => void }) {
+    return (
+        <React.Fragment>
+            {dayjs(isStartDate ? project.startDate : project.deadline).format('L')}
+            <IconButton size='small' sx={{ml: 1}} onClick={(event) => {
+                event.stopPropagation();
+                onEdit(project);
+            }}>
+                <Edit fontSize="inherit"/>
+            </IconButton>
+        </React.Fragment>
     );
 }
