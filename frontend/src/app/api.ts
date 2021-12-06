f7bb985e9ab8f704808cb5b09c577519cd378eb7
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2021-12-06 17:10:25 +0100

    Add using websocket connection in frontend for projects
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -2,6 +2,9 @@ import {baseUrl} from "../features/api/apiSlice";
 import ndjsonStream, {CancelCallback, Result} from "can-ndjson-stream";
 import {store} from "./store";
 import {selectToken} from "../features/auth/authSlice";
+import {Client} from "@stomp/stompjs";
+import {messageCallbackType} from "@stomp/stompjs/esm6/types";
+import {StompHeaders} from "@stomp/stompjs/esm6/stomp-headers";
 
 export async function subscribe<UpdateType>(
     path: string,
@@ -37,5 +40,57 @@ export async function subscribe<UpdateType>(
         };
     } else {
         throw new Error('no response body');
+    }
+}
+
+export const websocketClient = {
+    _stompClient: undefined as Client | undefined,
+    connect(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            let isPromiseFinished = false;
+            this._stompClient = new Client({
+                brokerURL: "ws://localhost:8085/stomp",
+                connectHeaders: {
+                    "access-token": store.getState().auth.token!!
+                },
+                onConnect: (frame) => {
+                    console.log("connected")
+                    console.log(frame)
+                    if (!isPromiseFinished) {
+                        isPromiseFinished = true;
+                        resolve();
+                    }
+                },
+                onDisconnect: (frame) => {
+                    console.log("disconnected")
+                    console.log(frame)
+                },
+                onWebSocketClose: (event) => {
+                    console.log("websocket closed")
+                    console.log(event)
+                    if (!isPromiseFinished) {
+                        isPromiseFinished = true;
+                        reject();
+                    }
+                },
+                onWebSocketError: (event) => {
+                    console.log("websocket error")
+                    console.log(event)
+                    if (!isPromiseFinished) {
+                        isPromiseFinished = true;
+                        reject();
+                    }
+                }
+            });
+            this._stompClient.activate();
+        });
+    },
+    subscribe(destination: string, callback: messageCallbackType, headers?: StompHeaders) {
+        if (this._stompClient) {
+            return this._stompClient.subscribe(`/user/topic${destination}`, callback, headers)
+        } else {
+            console.log("stomp client undefined");
+            throw Error("stomp client undefined")
+        }
     }
 }
