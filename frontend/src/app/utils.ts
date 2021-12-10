be22136bdc04c53bfc379d19b9e9be577d102291
Lars Duelfer <lars.duelfer@novatec-gmbh.de>
2021-12-10 08:27:09 +0000

    Fix wrong start dates of tasks in gant chart

--- frontend/src/app/utils.ts
+++ frontend/src/app/utils.ts
@@ -1,7 +1,7 @@
 export function parseDate(isoDate: string): Date {
     const splitDate = isoDate.split("-");
     const year = parseInt(splitDate[0]);
-    const month = parseInt(splitDate[1]);
+    const month = parseInt(splitDate[1])-1;
     const day = parseInt(splitDate[2]);
 
     return new Date(year, month, day);
