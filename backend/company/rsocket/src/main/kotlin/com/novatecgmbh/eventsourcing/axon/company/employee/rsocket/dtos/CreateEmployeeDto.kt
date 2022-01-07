9e5a040bfa58f0334c191784f55a24a4ddb28491
David Kronhardt <34436776+dakr0013@users.noreply.github.com>
2022-01-07 12:51:32 +0100

    Add rsocket mutations for project and company
    Signed-off-by: David Kronhardt <34436776+dakr0013@users.noreply.github.com>

---
+++ backend/company/rsocket/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/company/employee/rsocket/dtos/CreateEmployeeDto.kt
@@ -0,0 +1,14 @@
+package com.novatecgmbh.eventsourcing.axon.company.employee.rsocket.dtos
+
+import com.novatecgmbh.eventsourcing.axon.company.company.api.CompanyId
+import com.novatecgmbh.eventsourcing.axon.company.employee.api.CreateEmployeeCommand
+import com.novatecgmbh.eventsourcing.axon.company.employee.api.EmployeeId
+import com.novatecgmbh.eventsourcing.axon.user.api.UserId
+
+data class CreateEmployeeDto(
+    val identifier: EmployeeId,
+    val companyId: CompanyId,
+    val userId: UserId
+) {
+  fun toCommand() = CreateEmployeeCommand(identifier, companyId, userId)
+}
