c24f3b3780e8a07d7cc0c958c52eb7525772b7ee
Lars Duelfer <lars.duelfer@novatec-gmbh.de>
2022-01-28 10:01:17 +0000

    Rename api-gateway module to apis

---
+++ backend/apis/api-common/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+  id("com.novatecgmbh.commons-kotlin")
+  id("io.spring.dependency-management")
+  kotlin("plugin.spring")
+}
+
+group = "${group}.apis"
+
+dependencies {
+  implementation("com.novatecgmbh.eventsourcing.axon.common:auditing")
+  implementation("com.novatecgmbh.eventsourcing.axon.user:api")
+
+  implementation("org.axonframework:axon-spring-boot-starter")
+  implementation("org.axonframework.extensions.kotlin:axon-kotlin")
+
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+}
---
+++ backend/apis/websocket-rsocket/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+  id("com.novatecgmbh.commons-kotlin")
+  id("io.spring.dependency-management")
+  kotlin("plugin.spring")
+}
+
+group = "${group}.apis"
+
+dependencies {
+  implementation("com.novatecgmbh.eventsourcing.axon.common:api")
+  implementation("com.novatecgmbh.eventsourcing.axon.common:auditing")
+  implementation("com.novatecgmbh.eventsourcing.axon.project:api")
+  implementation("com.novatecgmbh.eventsourcing.axon.company:api")
+
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("io.projectreactor:reactor-core")
+  implementation("org.axonframework:axon-spring-boot-starter")
+  implementation("org.axonframework.extensions.kotlin:axon-kotlin")
+  implementation("org.axonframework.extensions.reactor:axon-reactor-spring-boot-starter")
+  implementation("org.axonframework.extensions.tracing:axon-tracing-spring-boot-starter")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-rsocket")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.security:spring-security-messaging")
+  implementation("org.springframework.security:spring-security-rsocket")
+
+  testImplementation("org.axonframework:axon-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
