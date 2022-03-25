d0333f05ea917f799542eda156cfae79742f79f5
Lars Duelfer <lars.duelfer@novatec-gmbh.de>
2022-03-25 13:34:03 +0000

    GraphQL API - allow to filter tasks by date range

--- backend/services/project/application/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/project/task/query/TaskProjectionRepository.kt
+++ backend/services/project/application/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/project/task/query/TaskProjectionRepository.kt
@@ -2,6 +2,7 @@ package com.novatecgmbh.eventsourcing.axon.project.task.query
 
 import com.novatecgmbh.eventsourcing.axon.project.project.api.ProjectId
 import com.novatecgmbh.eventsourcing.axon.project.task.api.TaskId
+import java.time.LocalDate
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -12,6 +13,14 @@ interface TaskProjectionRepository : JpaRepository<TaskProjection, TaskId> {
   @Query("select t from TaskProjection t left join fetch t.todos where t.projectId = :projectId")
   fun findAllByProjectId(@Param("projectId") projectId: ProjectId): List<TaskProjection>
 
-  @Query("select t from TaskProjection t left join fetch t.todos where t.projectId in :projectIds")
-  fun findAllByProjectIdIn(@Param("projectIds") projectIds: Set<ProjectId>): List<TaskProjection>
+  @Query(
+      "select t from TaskProjection t left join fetch t.todos " +
+          "where t.projectId in :projectIds " +
+          "and (cast(:from as date) is null or t.startDate > :from or t.endDate > :from) " +
+          "and (cast(:to as date) is null or t.startDate < :to or t.endDate < :to)")
+  fun findAllByProjectIdInAndDatesInRange(
+      @Param("projectIds") projectIds: Set<ProjectId>,
+      @Param("from") from: LocalDate?,
+      @Param("to") to: LocalDate?
+  ): List<TaskProjection>
 }
