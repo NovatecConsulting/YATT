668e60a0b5529e57e32657d9c2c9af72e0619131
Lars Duelfer <lars.duelfer@novatec-gmbh.de>
2022-03-31 15:10:08 +0000

    Add graphql subscription query for projects

--- backend/apis/graphql/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/project/project/graphql/ProjectController.kt
+++ backend/apis/graphql/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/project/project/graphql/ProjectController.kt
@@ -8,13 +8,16 @@ import java.util.concurrent.CompletableFuture
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.extensions.kotlin.queryMany
 import org.axonframework.extensions.kotlin.queryOptional
+import org.axonframework.messaging.responsetypes.ResponseTypes
 import org.axonframework.queryhandling.QueryGateway
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
+import reactor.core.publisher.Flux
 
 @Controller
 class ProjectController(val commandGateway: CommandGateway, val queryGateway: QueryGateway) {
@@ -30,6 +33,23 @@ class ProjectController(val commandGateway: CommandGateway, val queryGateway: Qu
   ): CompletableFuture<List<ProjectQueryResult>> =
       queryGateway.queryMany(
           MyProjectsQuery((user.principal as RegisteredUserPrincipal).identifier))
+
+  @SubscriptionMapping
+  fun projectsAndUpdates(
+      @AuthenticationPrincipal user: UsernamePasswordAuthenticationToken
+  ): Flux<ProjectQueryResult> {
+    val query =
+        queryGateway.subscriptionQuery(
+            MyProjectsQuery((user.principal as RegisteredUserPrincipal).identifier),
+            ResponseTypes.multipleInstancesOf(ProjectQueryResult::class.java),
+            ResponseTypes.instanceOf(ProjectQueryResult::class.java))
+
+    return query
+        .initialResult()
+        .flatMapMany { Flux.fromIterable(it) }
+        .concatWith(query.updates())
+        .doFinally { query.cancel() }
+  }
 
   @MutationMapping
   fun createProject(
