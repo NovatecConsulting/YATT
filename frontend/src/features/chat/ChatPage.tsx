d11106b2d0f5ba7da57d25b31328e0d752552281
David Kronhardt <david.kronhardt@novatec-gmbh.de>
2022-04-25 09:14:21 +0200

    Connect chat ui to backend
    Signed-off-by: David Kronhardt <david.kronhardt@novatec-gmbh.de>

--- frontend/src/features/chat/ChatPage.tsx
+++ frontend/src/features/chat/ChatPage.tsx
@@ -4,11 +4,38 @@ import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 // @ts-ignore
 import {MainContainer, ChatContainer, MessageList, Message, MessageInput} from '@chatscope/chat-ui-kit-react';
 import {Card} from "@mui/material";
+import {useEffect, useState} from "react";
+import {rsocket} from "../../app/rsocket";
+import {useParams} from "react-router-dom";
+import dayjs from "dayjs";
+import {useAppSelector} from "../../app/hooks";
+import {selectCurrentUser} from "../auth/usersSlice";
 
+export interface ChatMessage {
+    userIdentifier: string;
+    userName: string;
+    timestamp: string;
+    message: string;
+}
 
 export function ChatPage() {
+    const {projectId} = useParams<{ projectId: string }>();
+    const currentUser = useAppSelector(selectCurrentUser);
+    const [messages, setMessages] = useState([] as ChatMessage[]);
 
-    const handleSend = () => console.log("send pressed")
+    const handleSend = (innerHtml: String, textContent: String, innerText: String, nodes: NodeList) => {
+        rsocket.fireAndForget(`projects.${projectId}.chat.send`, innerText);
+    }
+
+    useEffect(() => {
+        const subscription = rsocket.subscribeUpdates<ChatMessage>(`projects.${projectId}.chat`, message => {
+            setMessages(messages => messages.concat(message))
+        });
+
+        return function cleanup() {
+            subscription.cancel();
+        };
+    }, [projectId]);
 
     return (
         <Scaffold>
@@ -16,39 +43,22 @@ export function ChatPage() {
                 <MainContainer style={styles}>
                     <ChatContainer>
                         <MessageList>
-                            <Message model={{
-                                // message: "Hello my friend",
-                                // sentTime: "just now",
-                                // sender: "Joe",
-                                direction: "incoming",
-                                position: "single",
-                                payload: <Message.CustomContent>
-                                    <strong>Joe</strong><br/>
-                                    Hello my friend<br/>
-                                    <div style={{textAlign: "right", fontWeight: "lighter"}}>12:00</div>
-                                </Message.CustomContent>
-                            }}/>
-                            <Message model={{
-                                // message: "Hello",
-                                // sentTime: "20:22",
-                                // sender: "Me",
-                                direction: "outgoing",
-                                position: "single",
-                                payload: <Message.CustomContent>
-                                    {/*<strong>Joe</strong><br/>*/}
-                                    Hello<br/>
-                                    <div style={{textAlign: "right", fontWeight: "lighter"}}>12:01</div>
-                                </Message.CustomContent>
-                            }}/>
-                            <Message model={{
-                                direction: "incoming",
-                                position: "single",
-                                payload: <Message.CustomContent>
-                                    <strong>Max</strong><br/>
-                                    Hi<br/>
-                                    <div style={{textAlign: "right", fontWeight: "lighter"}}>12:03</div>
-                                </Message.CustomContent>
-                            }}/>
+                            {
+                                messages.map(message => (
+                                    <Message model={{
+                                        direction: currentUser!!.identifier === message.userIdentifier ? "outgoing" : "incoming",
+                                        position: "single",
+                                        payload: <Message.CustomContent>
+                                            <strong>{message.userName}</strong><br/>
+                                            {message.message.replace(RegExp("^\""), "").replace(RegExp("\"$"), "")}<br/>
+                                            <div style={{
+                                                textAlign: "right",
+                                                fontWeight: "lighter"
+                                            }}>{dayjs(message.timestamp).format('LT')}</div>
+                                        </Message.CustomContent>
+                                    }}/>
+                                ))
+                            }
                         </MessageList>
                         <MessageInput attachButton={false} placeholder="Type message here" onSend={handleSend}/>
                     </ChatContainer>
