59826e9ad1ecddb5f515474f3aff5ec311796af9
Thomas Flad <thomas.flad@novatec-gmbh.de>
2022-06-23 09:28:26 +0200

    Temporary disable iss claim validation to enable support for mobile

--- backend/apis/graphql/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/application/config/SecurityConfig.kt
+++ backend/apis/graphql/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/application/config/SecurityConfig.kt
@@ -1,6 +1,7 @@
 package com.novatecgmbh.eventsourcing.axon.application.config
 
 import com.novatecgmbh.eventsourcing.axon.application.security.CustomUserAuthenticationConverter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
@@ -9,42 +10,58 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.JwtDecoders
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 class SecurityConfig(val userDetailsService: UserDetailsService) : WebSecurityConfigurerAdapter() {
 
-  override fun configure(http: HttpSecurity) {
-    http.cors()
-        .and()
-        .headers()
-        .httpStrictTransportSecurity()
-        .disable()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(STATELESS)
-        .and()
-        .csrf()
-        .disable()
-        .formLogin()
-        .disable()
-        .httpBasic()
-        .disable()
-        .exceptionHandling()
-        .and()
-        .authorizeRequests()
-        .antMatchers("/graphql/schema/**", "/graphiql/**")
-        .permitAll()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .oauth2ResourceServer()
-        .jwt()
-        .jwtAuthenticationConverter(customUserAuthenticationConverter())
-  }
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private val issuer: String? = null
 
-  @Bean
-  fun customUserAuthenticationConverter(): CustomUserAuthenticationConverter =
-      CustomUserAuthenticationConverter(userDetailsService)
+    override fun configure(http: HttpSecurity) {
+        http.cors()
+            .and()
+            .headers()
+            .httpStrictTransportSecurity()
+            .disable()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(STATELESS)
+            .and()
+            .csrf()
+            .disable()
+            .formLogin()
+            .disable()
+            .httpBasic()
+            .disable()
+            .exceptionHandling()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/graphql/schema/**", "/graphiql/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt()
+            .decoder(jwtDecoder())
+            .jwtAuthenticationConverter(customUserAuthenticationConverter())
+    }
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        val jwtDecoder: NimbusJwtDecoder = JwtDecoders.fromIssuerLocation(issuer) as NimbusJwtDecoder
+        jwtDecoder.setJwtValidator(DelegatingOAuth2TokenValidator(JwtTimestampValidator()))
+        return jwtDecoder
+    }
+
+    @Bean
+    fun customUserAuthenticationConverter(): CustomUserAuthenticationConverter =
+        CustomUserAuthenticationConverter(userDetailsService)
 }
--- backend/apis/rest/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/application/config/SecurityConfig.kt
+++ backend/apis/rest/src/main/kotlin/com/novatecgmbh/eventsourcing/axon/application/config/SecurityConfig.kt
@@ -1,6 +1,7 @@
 package com.novatecgmbh.eventsourcing.axon.application.config
 
 import com.novatecgmbh.eventsourcing.axon.application.security.CustomUserAuthenticationConverter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
@@ -9,41 +10,58 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.JwtDecoders
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 class SecurityConfig(val userDetailsService: UserDetailsService) : WebSecurityConfigurerAdapter() {
 
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private val issuer: String? = null
+
   @Throws(Exception::class)
   override fun configure(http: HttpSecurity) {
     http.cors()
-        .and()
-        .headers()
-        .httpStrictTransportSecurity()
-        .disable()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(STATELESS)
-        .and()
-        .csrf()
-        .disable()
-        .formLogin()
-        .disable()
-        .httpBasic()
-        .disable()
-        .exceptionHandling()
-        .and()
-        .authorizeRequests()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .oauth2ResourceServer()
-        .jwt()
-        .jwtAuthenticationConverter(customUserAuthenticationConverter())
+      .and()
+      .headers()
+      .httpStrictTransportSecurity()
+      .disable()
+      .and()
+      .sessionManagement()
+      .sessionCreationPolicy(STATELESS)
+      .and()
+      .csrf()
+      .disable()
+      .formLogin()
+      .disable()
+      .httpBasic()
+      .disable()
+      .exceptionHandling()
+      .and()
+      .authorizeRequests()
+      .anyRequest()
+      .authenticated()
+      .and()
+      .oauth2ResourceServer()
+      .jwt()
+      .decoder(jwtDecoder())
+      .jwtAuthenticationConverter(customUserAuthenticationConverter())
+  }
+
+  @Bean
+  fun jwtDecoder(): JwtDecoder {
+    val jwtDecoder: NimbusJwtDecoder = JwtDecoders.fromIssuerLocation(issuer) as NimbusJwtDecoder
+    jwtDecoder.setJwtValidator(DelegatingOAuth2TokenValidator(JwtTimestampValidator()))
+    return jwtDecoder
   }
 
   @Bean
   fun customUserAuthenticationConverter(): CustomUserAuthenticationConverter =
-      CustomUserAuthenticationConverter(userDetailsService)
+    CustomUserAuthenticationConverter(userDetailsService)
+
 }
